e783a59f071162564a98064a25f82fb1117006f0
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-08 16:32:42 +0300

    animate modal

--- E-ticaret/js/main.js
+++ E-ticaret/js/main.js
@@ -2,10 +2,20 @@
 $(document).ready(function () {
     $("#btn_login").click(function () {
         console.log("a")
-        $("#login_Modal").show();
-        $("#login_ModalBg").show();
+        $("#login_Modal").fadeIn();
+        $("#login_ModalBg").fadeIn();
     })
     $("#login_Modal .close").click(function(){
-        
+        $("#login_Modal").fadeOut();
+        $("#login_ModalBg").fadeOut();
+    })
+    $("#login_ModalBg").click(function(){
+        $("#login_Modal").animate({
+            left:"36%",
+            top:"6%"
+        },100).animate({
+            left:"35%",
+            top:"5%"
+        },100)
     })
 })
