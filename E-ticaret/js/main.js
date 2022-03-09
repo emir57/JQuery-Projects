2ec8dd275b718dc87a377b4bacbb1b2f8aa1cd57
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-09 14:21:40 +0300

    refactoring animate register modal

--- E-ticaret/js/main.js
+++ E-ticaret/js/main.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     let login_Modal = $("#login_Modal");
     let register_Modal = $("#register_Modal");
     let modalBg = $("#modalBg");
+
     btn_login.click(function () {
         login_Modal.fadeIn();
         login_Modal.animate({
@@ -16,11 +17,12 @@ $(document).ready(function () {
     btn_register.click(function () {
         register_Modal.fadeIn();
         register_Modal.animate({
-            top:"5%",
+            top:"2%",
             opacity:100
         },200)
         modalBg.fadeIn();
     })
+
     $("#login_Modal .close").click(function(){
         login_Modal.animate({
             top:"3%",
@@ -31,15 +33,17 @@ $(document).ready(function () {
     })
     $("#register_Modal .close").click(function(){
         register_Modal.animate({
-            top:"3%",
+            top:"0%",
             opacity:0
         },200)
         register_Modal.fadeOut();
         modalBg.fadeOut();
     })
+
     modalBg.click(function(){
         $("#login_Modal .modal-dialog").addClass("redborder");
         let leftp = login_Modal.css("left").split("px")[0];
+        
         login_Modal.animate({
             left:"36%",
         },100).animate({
@@ -47,6 +51,7 @@ $(document).ready(function () {
         },100,function(){
             $("#login_Modal .modal-dialog").removeClass("redborder");
         })
+
         register_Modal.animate({
             left:"36%",
         },100).animate({
