e207aee29196b76727bb8d7f9aeaf6f39085b39d
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-09 14:27:15 +0300

    js

--- E-ticaret/js/main.js
+++ E-ticaret/js/main.js
@@ -1,63 +1,69 @@
+/*
+$(document).ready(function(){
+
+})
+*/
 
 $(document).ready(function () {
-    let btn_login = $("#btn_login");
-    let btn_register = $("#btn_register");
-    let login_Modal = $("#login_Modal");
-    let register_Modal = $("#register_Modal");
-    let modalBg = $("#modalBg");
-
+    var btn_login = $("#btn_login");
+    var btn_register = $("#btn_register");
+    var login_Modal = $("#login_Modal");
+    var register_Modal = $("#register_Modal");
+    var modalBg = $("#modalBg");
     btn_login.click(function () {
         login_Modal.fadeIn();
         login_Modal.animate({
-            top:"5%",
-            opacity:100
-        },200)
+            top: "5%",
+            opacity: 100
+        }, 200)
         modalBg.fadeIn();
     })
     btn_register.click(function () {
         register_Modal.fadeIn();
         register_Modal.animate({
-            top:"2%",
-            opacity:100
-        },200)
+            top: "2%",
+            opacity: 100
+        }, 200)
         modalBg.fadeIn();
     })
-
-    $("#login_Modal .close").click(function(){
+    $("#login_Modal .close").click(function () {
         login_Modal.animate({
-            top:"3%",
-            opacity:0
-        },200)
+            top: "3%",
+            opacity: 0
+        }, 200)
         login_Modal.fadeOut();
         modalBg.fadeOut();
     })
-    $("#register_Modal .close").click(function(){
+    $("#register_Modal .close").click(function () {
         register_Modal.animate({
-            top:"0%",
-            opacity:0
-        },200)
+            top: "0%",
+            opacity: 0
+        }, 200)
         register_Modal.fadeOut();
         modalBg.fadeOut();
     })
 
-    modalBg.click(function(){
+    modalBg.click(function () {
         $("#login_Modal .modal-dialog").addClass("redborder");
         let leftp = login_Modal.css("left").split("px")[0];
-        
+
         login_Modal.animate({
-            left:"36%",
-        },100).animate({
-            left:"35%",
-        },100,function(){
+            left: "36%",
+        }, 100).animate({
+            left: "35%",
+        }, 100, function () {
             $("#login_Modal .modal-dialog").removeClass("redborder");
         })
 
         register_Modal.animate({
-            left:"36%",
-        },100).animate({
-            left:"35%",
-        },100,function(){
+            left: "36%",
+        }, 100).animate({
+            left: "35%",
+        }, 100, function () {
             $("#login_Modal .modal-dialog").removeClass("redborder");
         })
     })
+})
+$(document).ready(function () {
+    
 })
